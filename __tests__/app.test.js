18bfbd18818cea6f0643ab5999ca39d2a89267b5
bwapes <benwaples@gmail.com>
2020-09-28 15:52:56 -0700

    configure git enviroment

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -39,5 +39,31 @@ describe('tardygram routes', () => {
 
     expect(foundUser.body).toEqual(response.body);
   });
+
+  it('should verify a user via GET', async() => {
+    const agent = request.agent(app);
+    const signIn = await agent
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'ben@ben.com',
+        password: 'password'
+      });
+    
+    const response = await agent
+      .get('/api/v1/auth/verify');
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'ben@ben.com'
+    });
+      
+    const responseWithoutUser = await request(app)
+      .get('/api/v1/auth/verify');
+
+    expect(responseWithoutUser.body).toEqual({
+      status: 500,
+      message: 'jwt must be provided'
+    });
+  });
 });
  
