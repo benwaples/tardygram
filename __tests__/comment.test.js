197c4ce371d3c9e4b45939c3925b7d062055db8f
bwapes <benwaples@gmail.com>
2020-10-04 20:47:09 -0700

    test pass

---
+++ __tests__/comment.test.js
@@ -0,0 +1,11 @@
+require('../lib/data/data-helper');
+const { getAgent } = require('../lib/data/data-helper');
+const request = require('supertest');
+const app = require('../lib/app');
+const Comment = require('../lib/models/comment');
+
+describe('Comment Routes', () => {
+  it('should test my patience', () => {
+    
+  });
+});
